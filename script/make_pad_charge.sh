7961830e4e8b92dcbdbbb059d04328091bbfccb6
suvorov21 <suvorov@inr.ru>
2017-11-10 18:13:01 +0100

    Fix all the problems

--- script/make_pad_charge.sh
+++ script/make_pad_charge.sh
@@ -1,6 +1,6 @@
 cp ~/suvorov/macros/pad_charge.C ~/INSTALLFCC/macros
 cd ~/INSTALLFCC/macros
-make SRC="mydict.C pad_scan.C"  EXE=pad_charge.EXE
+make SRC="mydict.C pad_charge.C"  EXE=pad_charge.EXE
 rm pad_charge.C
 mv pad_charge.EXE ~/suvorov/script
 cd ~/suvorov/
