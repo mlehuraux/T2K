26593f083cde2a42910ac7ace04df9414ab324af
suvorov21 <suvorov@inr.ru>
2017-11-10 15:01:31 +0100

    Move calculations to /devel/temp

--- script/acq_to_root_separate_fem.sh
+++ script/acq_to_root_separate_fem.sh
@@ -5,6 +5,8 @@ array=($(cat ~/suvorov/FileList/acq/run5206.list))
 
 merge=true
 
+cd ~/INSTALLFCC/DataWriter_devel/
+mkdir temp
 # loop over subruns
 for ((i=0; i<${#array[@]}; i++));
 do
@@ -13,28 +15,32 @@ do
   f_acq=${path##*/}
   f_root=${f_acq%.*}".root"
   echo "Processing file: $path"
-  cp $path ~/suvorov/data_temp/$f_acq
+  cp $path ~/INSTALLFCC/DataWriter_devel/temp/$f_acq
   
   # loop over fems
-  for ((j=0; j<7; j++));
+  for ((j=0; j<1; j++));
   do
     f_root_new=${f_acq%.*}"_fem"$j".root"
     echo "**********************************************"
     echo "Creating $f_root_new"
     echo "**********************************************"
-    ~/INSTALLFCC/DataWriter_devel/bin/acqToRootConverterExe -t $j -m 1 -f  $f_acq --noxml
-    mv $f_root ../data_root/$f_root_new
+    
+    ./bin/acqToRootConverterExe -t $j -m 1 -f  "temp/$f_acq" --noxml
+    ls -l temp/
+    mv temp/$f_root ~/suvorov/data_root/$f_root_new
   done
 
-  rm ~/suvorov/data_temp/$f_acq
+  rm ~/INSTALLFCC/DataWriter_devel/temp/$f_acq
 done
 
+rm -r temp
+
+cd ~/suvorov/
 if  $merge
 then
-  cd ../
   echo "Merging the subruns"
-   loop over fems
-  for ((j=0; j<7; j++));
+  # loop over fems
+  for ((j=0; j<1; j++));
   do
     hadd "data_root/${f_acq%.*.*}_fem$j.root" "data_root/${f_acq%.*.*}."*"_fem$j.root"
   done
