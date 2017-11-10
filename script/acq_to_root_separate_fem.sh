187204b9faf47c39ac51d2bfbffb1a72b4c8d648
suvorov21 <suvorov@inr.ru>
2017-11-10 11:08:49 +0100

    Create script for fem by fem to root conversion

--- script/acq_to_root_separate_fem.sh
+++ script/acq_to_root_separate_fem.sh
@@ -1,3 +1,46 @@
-array=($(cat dev/FileList/runs/data_6B_run2a.list))
-echo "${array[1]}"
-echo "${array[4]}"
+path_ini=$(pwd)
+cd ~/suvorov/data_temp/
+
+array=($(cat ~/suvorov/FileList/acq/run5206.list))
+
+merge=true
+
+# loop over subruns
+for ((i=0; i<${#array[@]}; i++));
+do
+  echo "$i"
+  path=${array[$i]}
+  f_acq=${path##*/}
+  f_root=${f_acq%.*}".root"
+  echo "Processing file: $path"
+  cp $path ~/suvorov/data_temp/$f_acq
+  
+  # loop over fems
+  for ((j=0; j<7; j++));
+  do
+    f_root_new=${f_acq%.*}"_fem"$j".root"
+    echo "**********************************************"
+    echo "Creating $f_root_new"
+    echo "**********************************************"
+    ~/INSTALLFCC/DataWriter_devel/bin/acqToRootConverterExe -t $j -m 1 -f  $f_acq --noxml
+    mv $f_root ../data_root/$f_root_new
+  done
+
+  rm ~/suvorov/data_temp/$f_acq
+done
+
+if  $merge
+then
+  cd ../
+  echo "Merging the subruns"
+   loop over fems
+  for ((j=0; j<7; j++));
+  do
+    hadd "data_root/${f_acq%.*.*}_fem$j.root" "data_root/${f_acq%.*.*}."*"_fem$j.root"
+  done
+  rm "data_root/RUN"*"."*"_fem"*".root"
+fi
+
+cd $path_ini
+
+
