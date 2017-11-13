91a2bc5988fc1c38ca68d95d58586228c565e269
suvorov21 <suvorov@inr.ru>
2017-11-13 17:21:10 +0100

    make general compiler script

--- script/make_pad_charge.sh
+++
@@ -1,6 +0,0 @@
-cp ~/suvorov/macros/pad_charge.C ~/INSTALLFCC/macros
-cd ~/INSTALLFCC/macros
-make SRC="mydict.C pad_charge.C"  EXE=pad_charge.EXE
-rm pad_charge.C
-mv pad_charge.EXE ~/suvorov/script
-cd ~/suvorov/
