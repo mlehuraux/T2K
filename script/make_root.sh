5517a8917ab836290c504c504e441247cf236df3
suvorov21 <suvorov@inr.ru>
2017-11-16 15:44:34 +0100

    Add local compilation

--- script/make_root.sh
+++ script/make_root.sh
@@ -1,3 +1,4 @@
+path_ini=$(pwd)
 src_path=$1
 src_file=${src_path##*/}
 file_exe=${src_file%.*}".EXE"
@@ -6,10 +7,19 @@ echo "*********************************************************************"
 echo "*********   BUILDING $src_path TO $file_exe **********"
 echo "*********************************************************************"
 echo "*********************************************************************"
+local=true
+if $local
+then 
+  compile_path=~/T2K/ILC_TPC/ilc_macros/
+  out=~/T2K/ILC_TPC/script/
+else
+  compile_path=~/INSTALLFCC/macros/
+  out=~/suvorov/script/
+fi
 
-cp $src_path ~/INSTALLFCC/macros
-cd ~/INSTALLFCC/macros
+cp $src_path $compile_path$src_file
+cd $compile_path
 make SRC="mydict.C $src_file"  EXE=$file_exe
 rm $src_file
-mv $file_exe ~/suvorov/script
-cd ~/suvorov/
+mv $file_exe $out
+cd $path_ini
