844bd2675a01b497a3c559127a146bb6191c64c6
suvorov21 <suvorov@inr.ru>
2017-11-29 17:47:19 +0100

    create the list before running conversion

--- script/multiple_acq_to_root.sh
+++ script/multiple_acq_to_root.sh
@@ -1,7 +1,8 @@
-start=5148
-for ((z=0; z<13; ++z));
+start=5162
+for ((z=0; z<6; ++z));
 do
   run=$(($start+$z))
   echo "$run"
+  ls /media/ILC-7M_DATA_04/data/RUN_0$run*.acq > FileList/acq/run$run.list
   source script/acq_to_root_separate_fem.sh $run
 done
