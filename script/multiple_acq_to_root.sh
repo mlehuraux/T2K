c819da0dad836c96be38b15cbed0dd5e041f21f2
suvorov21 <suvorov@inr.ru>
2017-12-20 19:08:40 +0100

    Add possibility to take acq data from local folder

--- script/multiple_acq_to_root.sh
+++ script/multiple_acq_to_root.sh
@@ -1,8 +1,9 @@
-start=5162
-for ((z=0; z<6; ++z));
+start=4098
+for ((z=0; z<12; ++z));
 do
   run=$(($start+$z))
   echo "$run"
-  ls /media/ILC-7M_DATA_04/data/RUN_0$run*.acq > FileList/acq/run$run.list
+  #ls /media/ILC-7M_DATA_04/data/RUN_0$run*.acq > FileList/acq/run$run.list
+  ls ~/suvorov/data_acq/RUN_0$run*.acq > FileList/acq/run$run.list
   source script/acq_to_root_separate_fem.sh $run
 done
