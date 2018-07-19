74305ee0de5df66b7a148cdb846565cf8123435a
suvorov21 <suvorov@inr.ru>
2018-07-19 19:27:22 +0200

    modify scripts for T2K files

--- script/acq_to_root_separate_fem.sh
+++ script/acq_to_root_separate_fem.sh
@@ -1,9 +1,7 @@
 path_ini=$(pwd)
-cd ~/suvorov/data_temp/
 run=$1
-array=($(cat ~/suvorov/FileList/acq/run$run.list))
 SOFT="/local/home/ssuvorov/T2K/ILC_TPC/DataWriter_devel"
-#SOFT="~/INSTALLFCC/DataWriter_devel/"
+array=($(cat $SOFT/../tmp/run$run.list))
 
 merge=true
 
@@ -19,40 +17,21 @@ do
   cp $path $SOFT/temp/$f_acq
   source setup.sh
 
-  # loop over fems
-  for ((j=0; j<7; j++));
-  do
-    f_root_new=${f_acq%.*}"_fem"$j".root"
-    echo "**********************************************"
-    echo "Creating $f_root_new"
-    echo "**********************************************"
-    k=1
-    if (($j==1))
-      then
-      k=2
-    fi
-    ./bin/acqToRootConverterExe -t $j -m $k -f  "temp/$f_acq" --noxml
-    ls -l temp/
-    mv temp/$f_root ~/$f_root_new
-  done
+  ./bin/acqToRootConverterExe -t 0 -m 1 -f  "temp/$f_acq" --noxml
+  ls -l temp/
+  mv temp/$f_root $SOFT/../../data/T2K_TPC/data_root/$f_root
 
   rm $SOFT/temp/$f_acq
 done
 
 rm -r temp
 
-cd ~/suvorov/
 if  $merge
 then
+  cd $SOFT/../../data/T2K_TPC/data_root
   echo "Merging the subruns"
-
-  # loop over fems
-  for ((j=0; j<7; j++));
-  do
-    rm data_root/${f_acq%.*.*}_fem$j.root
-    hadd "data_root/${f_acq%.*.*}_fem$j.root" "data_root/${f_acq%.*.*}."*"_fem$j.root"
-  done
-  rm "data_root/RUN"*"."*"_fem"*".root"
+  hadd -f "${f_acq%.*.*}.root" "${f_acq%.*.*}."*".root"
+  rm "${f_acq%.*.*}."*".root"
 fi
 
 cd $path_ini
--- script/multiple_acq_to_root.sh
+++ script/multiple_acq_to_root.sh
@@ -1,9 +1,16 @@
-start=4098
-for ((z=0; z<12; ++z));
+start=135
+end=139
+project="/local/home/ssuvorov/T2K/ILC_TPC/"
+mkdir $project/tmp
+for ((z=start; z<=end; ++z));
 do
-  run=$(($start+$z))
-  echo "$run"
-  #ls /media/ILC-7M_DATA_04/data/RUN_0$run*.acq > FileList/acq/run$run.list
-  ls ~/suvorov/data_acq/RUN_0$run*.acq > FileList/acq/run$run.list
-  source script/acq_to_root_separate_fem.sh $run
+  run=$(((z)))
+  if [ $run -ne 133 ] && [ $run -ne 134 ] && [ $run -ne 149 ] && [ $run -ne 150 ] && [ $run -ne 148 ] && [ $run -ne 140 ]
+  then
+    echo "Working on run $run"
+    #ls /media/ILC-7M_DATA_04/data/RUN_0$run*.acq > FileList/acq/run$run.list
+    ls $project/../data/T2K_TPC/data_acq/RUN_00$run*.acq > $project/tmp/run$run.list
+    source $project/script/acq_to_root_separate_fem.sh $run
+  fi
 done
+rm -r $project/tmp
