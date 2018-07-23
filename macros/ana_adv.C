918515a5f6f84d85be588417bccc1c1c0290f00c
suvorov21 <suvorov@inr.ru>
2018-07-23 19:48:16 +0200

    new algorithm to add nearby hits implemented

--- macros/ana_adv.C
+++ macros/ana_adv.C
@@ -30,18 +30,25 @@ void ana_adv() {
   }
 
   int thr  = 100.;
+  int thr2 = 20.;
   double alpha = 0.7;
-  double tolerance = 3.;
+  int tolerance_pads = 3;
+  int tolerance_time = 3;
 
-  TCanvas *c1 = new TCanvas("c1","evadc",900,700);
+  TCanvas *c1 = new TCanvas("c1","evadc", 0, 0, 900,700);
+  //TCanvas *c2 = new TCanvas("c2","evadc", 900, 0, 900,700);
+  //TCanvas *c3 = new TCanvas("c3","evadc", 900, 700, 900,700);
   TString prefix = "~/T2K/figure/T2KTPC/";
   TH2F* PadDisplayGeo=new TH2F("PadDisplayGeo","X vs Y of hits",50,-17.15,17.15, 38,-18.13,18.13);
+  TH2F* PadDisplayH=new TH2F("PadDisplay","I vs J of hits",38,-1.,37.,50,-1.,49.);
+  TH2F* PadDisplayH1=new TH2F("PadDisplay","I vs J of hits",38,-1.,37.,50,-1.,49.);
 
   TH1F* ClusterCharge     = new TH1F("cluster_charge","Cluster charge",150,0,7000);
   TH1F* ClusterNormCharge = new TH1F("cluster_norm_charge","Truncated mean energy deposit",150,0,4000);
 
   TH1F* TimeMaximum       = new TH1F("time_max", "time of the maximum", 530, 0., 530.);
   TH1F* TimeMaximumL      = new TH1F("time_maxL", "time of the maximum", 100, 0., 530.);
+  TH1F* TimeMaximumT      = new TH1F("time_maxL", "time of the maximum", 530, 0., 530.);
 
   TH2F* PadDisplayHIT     = new TH2F("PadDisplay","I vs J of hits",38,-1.,37.,50,-1.,49.);
   TH2F* PadDisplayMAX     = new TH2F("PadDisplay","I vs J of hits",38,-1.,37.,50,-1.,49.);
@@ -53,6 +60,11 @@ void ana_adv() {
   Int_t total_events = 0;
   Int_t sel_events = 0;
   Int_t filled_events = 0;
+
+  vector< vector<int> > PadDisplay;
+  vector< vector<int> > PadTime;
+  vector< vector<int> > PadDisplayNew;
+  vector< vector<vector<short> > > PadDisplayV;
 
   //************************************************************
   //****************** LOOP OVER FILES  ************************
@@ -120,6 +132,7 @@ void ana_adv() {
     t->SetBranchAddress("PadphysChannels", &listOfChannels );
     t->SetBranchAddress("PadADCvsTime"   , &listOfSamples );
 
+
     if (Nevents<=0) Nevents=t->GetEntries();
     if (Nevents>t->GetEntries()) Nevents=t->GetEntries();
 
@@ -133,14 +146,32 @@ void ana_adv() {
         cout <<"."<<flush;
 
       t->GetEntry(ievt);
+      PadDisplayH->Reset();
+      PadDisplayH1->Reset();
 
       // clean for the next event
       PadDisplayGeo->Reset();
+      PadDisplayNew.clear();
+      PadDisplay.clear();
+      PadTime.clear();
 
-      vector< vector<int> > PadDisplay;
       PadDisplay.resize(Jmax+1);
       for (int z=0; z <= Jmax; ++z)
         PadDisplay[z].resize(Imax+1, 0);
+
+      PadTime.resize(Jmax+1);
+      for (int z=0; z <= Jmax; ++z)
+        PadTime[z].resize(Imax+1, 0);
+
+      PadDisplayNew.resize(Jmax+1);
+      for (int z=0; z <= Jmax; ++z)
+        PadDisplayNew[z].resize(Imax+1, 0);
+
+      PadDisplayV.resize(Jmax+1);
+      for (int z=0; z <= Jmax; ++z)
+        PadDisplayV[z].resize(Imax+1);
+
+
 
 
       //************************************************************
@@ -161,12 +192,18 @@ void ana_adv() {
           }
         }
 
+        PadDisplayV[(*jPad)[chan]][(*iPad)[chan]] = (*listOfSamples)[ic];
+
         if (adcmax<0) continue; // remove noise
         if ((*iPad)[chan] == Imin || (*iPad)[chan] == Imax ||
             (*jPad)[chan] == Jmin || (*jPad)[chan] == Jmax)
             continue;
 
         PadDisplay[(*jPad)[chan]][(*iPad)[chan]] = adcmax;
+        PadTime[(*jPad)[chan]][(*iPad)[chan]] = it_max;
+
+        PadDisplayH1->Fill((*iPad)[chan],(*jPad)[chan], adcmax);
+
         if (chan < 1728)
           PadDisplayGeo->Fill((*xPad)[chan],(*yPad)[chan], adcmax);
 
@@ -197,16 +234,91 @@ void ana_adv() {
       // trigger position study
       TriggerX->Fill(-16.45*k + b);
 
-      // delete everything besides track
-
-
       //************************************************************
       //*************2nd LOOP OVER CHANNELS ************************
       //************************************************************
-      for (uint ic=0; ic< listOfChannels->size(); ic++){
-        int chan = (*listOfChannels)[ic];
+      int time_bin  = -1;
+      int first_row = -1;
+      int last_row  = -1;
+
+      vector<pair<int, int> > track;
+      vector<int> track_time;
+
+      for (int j = 0; j <= Jmax; j++){
+        float closest_dist = 5.;
+        bool hit = false;
+        Double_t y = k * PadDisplayGeo->GetXaxis()->GetBinCenter(j+2) + b;
+        pair<int, int> closest;
+        time_bin  = -1;
+        for (int i = 0; i <= Imax; i++){
+          if (abs(PadDisplayGeo->GetYaxis()->GetBinCenter(i+2) - y) < closest_dist && PadDisplay[j][i]) {
+            closest_dist = abs(PadDisplayGeo->GetYaxis()->GetBinCenter(i+2) - y);
+            closest = make_pair(j, i);
+            time_bin = PadTime[j][i];
+
+            hit = true;
+
+          }
+        }
+        if (first_row == -1 && hit)
+          first_row = j;
+
+        if (hit)
+          last_row = j;
+
+        // end of the track == 2 empty rows
+        if (track.size() > last_row-first_row+1) break;
+
+        if (first_row != -1) {
+          track.push_back(closest);
+          // FIXME EXTREME HARDCODING
+          // if no time, but first_row defined--> previous value should exist
+          if (time_bin != -1)
+            track_time.push_back(time_bin);
+          else track_time.push_back(track_time[track_time.size()-1]);
+        }
 
       } // 2nd loop over channels
+
+      // look at tracks through MM / cathod
+      if (!track.size()) continue;
+      if (track[0].first > Jmin + 6 && abs(track[0].second-17.5) < 14)
+        TimeMaximumT->Fill(track_time[0]);
+      if (track[track.size()-1].first < Jmax - 6 && abs(track[track.size()-1].second-17.5) < 14)
+        TimeMaximumT->Fill(track_time[track.size()-1]);
+
+      // request only long tracks
+      if (track.size() < 10)
+        continue;
+
+      // loop over track
+      for (UInt_t row = 0; row < track.size(); ++row) {
+        // loop over near pads
+        int i_start = max(1, track[row].second - tolerance_pads);
+        int i_end   = min(Imax - 1, track[row].second + tolerance_pads);
+        int j = track[row].first;
+
+        // don't want to deal with last and pre-last row
+        if (j >= Jmax - 1)
+          break;
+
+        for (int i = i_start; i <= i_end; ++i) {
+          int adc_max = -1;
+
+          // loop over time bins
+          int time_min = max(0, track_time[row] - tolerance_time);
+          int time_max = min(track_time[row] + tolerance_time, 511);
+          for (int time = time_min; time <= time_max; ++time) {
+            if (PadDisplayV[j][i][time] > max(thr2, adc_max))
+              adc_max = PadDisplayV[j][i][time];
+          } // loop over time bins
+
+          if (adc_max != -1) {
+            PadDisplayNew[j][i] = adc_max;
+            PadDisplayH->Fill(i, j, adc_max);
+          }
+        } // loop over pads
+      } // loop over track
 
       // charge per cluster study
       vector<Float_t > cluster_charge;
@@ -215,19 +327,21 @@ void ana_adv() {
         Int_t cluster = 0;
         Int_t pads_per_cluster = 0;
         for (Int_t it_i = 0; it_i < Imax; ++it_i) {
-          cluster += PadDisplay[it_j][it_i];
-          if (PadDisplay[it_j][it_i] > 0) {
+          cluster += PadDisplayNew[it_j][it_i];
+          if (PadDisplayNew[it_j][it_i] > 0) {
             ++pads_per_cluster;
             PadDisplayHIT->Fill(it_i, it_j, 1);
+            //cout << "   " << it_i << "   " << PadDisplayNew[it_j][it_i];
           }
-          PadDisplayMAX->Fill(it_i, it_j, PadDisplay[it_j][it_i]);
+          PadDisplayMAX->Fill(it_i, it_j, PadDisplayNew[it_j][it_i]);
         }
-
-        PadsPerCluster->Fill(pads_per_cluster);
+        //cout << endl;
 
         if (cluster != 0) {
+          PadsPerCluster->Fill(pads_per_cluster);
           ClusterCharge->Fill(cluster);
           cluster_charge.push_back(cluster);
+          //cout << cluster << endl;
         }
       }
 
@@ -241,6 +355,18 @@ void ana_adv() {
 
       ClusterNormCharge->Fill(norm_cluster);
 
+      /*c1->cd();
+      PadDisplayH1->Draw("colz");
+      gPad->Update();
+      c2->cd();
+      PadDisplayH->Draw("colz");
+      gPad->Update();
+      c3->cd();
+      PadDisplayGeo->Draw("colz");
+      gPad->Update();
+      char c[10];
+      cin >> c;*/
+
 
     } // loop over events
   } // loop over files
@@ -248,7 +374,7 @@ void ana_adv() {
   gStyle->SetOptStat("RMne");
 
   ClusterCharge->Draw();
-  c1->Print((prefix+"ClusterCharge.pdf").Data());
+  c1->Print((prefix+"ClusterCharge_adv.pdf").Data());
 
 
   TF1* f1 = new TF1("f1", "( (x < [1] ) ? [0] * TMath::Gaus(x, [1], [2]) : [0] * TMath::Gaus(x, [1], [3]))", 0, 1300);
@@ -260,29 +386,31 @@ void ana_adv() {
   ClusterNormCharge->Fit("f1");
 
   ClusterNormCharge->Draw();
-  c1->Print((prefix+"ClusterChargeNorm.pdf").Data());
+  c1->Print((prefix+"ClusterChargeNorm_adv.pdf").Data());
 
   TimeMaximum->Draw();
-  c1->Print((prefix+"TimeMaximum.pdf").Data());
+  c1->Print((prefix+"TimeMaximum_adv.pdf").Data());
   TimeMaximumL->Draw();
-  c1->Print((prefix+"TimeMaximumL.pdf").Data());
+  c1->Print((prefix+"TimeMaximumL_adv.pdf").Data());
+  TimeMaximumT->Draw();
+  c1->Print((prefix+"TimeMaximumT_adv.pdf").Data());
 
   gStyle->SetOptStat(0);
 
   PadDisplayHIT->Draw("colz");
-  c1->Print((prefix+"pad_scan_hit_track.pdf").Data());
+  c1->Print((prefix+"pad_scan_hit_track_adv.pdf").Data());
   PadDisplayMAX->Draw("colz");
-  c1->Print((prefix+"pad_scan_max_track.pdf").Data());
+  c1->Print((prefix+"pad_scan_max_track_adv.pdf").Data());
 
   TriggerX->Draw();
-  c1->Print((prefix+"TriggerX.pdf").Data());
+  c1->Print((prefix+"TriggerX_adv.pdf").Data());
 
   PadsPerCluster->Draw();
-  c1->Print((prefix+"PadsPerCluster.pdf").Data());
+  c1->Print((prefix+"PadsPerCluster_adv.pdf").Data());
 
   PadDisplayMAX->Divide(PadDisplayHIT);
   PadDisplayMAX->Draw("colz");
-  c1->Print((prefix+"AvChargePerHit.pdf").Data());
+  c1->Print((prefix+"AvChargePerHit_adv.pdf").Data());
 
   cout << "Total events number     : " << total_events << endl;
   cout << "Not empty events number : " << filled_events << endl;
