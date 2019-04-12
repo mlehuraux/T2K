d7a7f341736a8ddbbb76bf402f63f7b835c077b7
Ryo Yonamine <ryo.yonamine@cea.fr>
2019-04-12 11:25:53 +0200

    .sh file to source to set up correct lib path

---
+++ InitEnvironment.sh
@@ -0,0 +1,6 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/harpo/PROJETS/T2K/DataWriter_devel/lib
+
+#In case you have the error related to dictionnary module 
+#cd macros/ilc/
+#rootcint -f  mydict.C -c LinkDef.h
+#cd ../../
