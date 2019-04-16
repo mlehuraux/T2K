e6c205ddd116cacc21161c1e8ca78b54657b9cf1
Ryo Yonamine <ryo.yonamine@cea.fr>
2019-04-16 16:26:01 +0200

    Update

--- InitEnvironment.sh
+++ InitEnvironment.sh
@@ -4,3 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/harpo/PROJETS/T2K/DataWriter_devel
 #cd macros/ilc/
 #rootcint -f  mydict.C -c LinkDef.h
 #cd ../../
+
+#In case you wanna compile macros 
+#make SRC=hit_map.C
