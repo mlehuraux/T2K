a29aeb0509ced4ee9b1b438fdf8a83c91d4c3418
Ryo Yonamine <ryo.yonamine@cea.fr>
2019-04-18 16:43:42 +0200

    Starting mapping

---
+++ T2KNewElectronics/T2KConstants.h
@@ -0,0 +1,24 @@
+// #include required here
+#ifndef T2KConstants_h
+#define T2KConstants_h
+
+#include <cmath>
+#include "math.h"
+#include <string>
+
+using namespace std;
+
+// To access constant PI
+#define _USE_MATH_DEFINES
+
+// Numbers of #
+namespace n {
+
+    static const int arc = 4;
+    static const int chip = 4;
+    static const int bins = 72;
+    static const int samples = 50;
+
+}
+
+#endif
