86bfa5e8b513548e5518c1544e18cde5c3f792b0
Marion LEHURAUX <marion.lehuraux@hotmail.fr>
2019-05-09 10:26:03 +0200

    Update

--- T2KNewElectronics/Analysis_Display_Soft/run.sh
+++ T2KNewElectronics/Analysis_Display_Soft/run.sh
@@ -22,14 +22,14 @@ export here="/home/mlehuraux/GitLab/T2K/T2KNewElectronics/Analysis_Display_Soft"
 #cd $here/treeBuilder
 #make
 #cd $here/bin
-./treeBuilder R2019_05_02-11_48_23-000
+#./treeBuilder R2019_05_02-11_48_23-000
 
 #################
 # Pre-treatment #
 #################
-#cd $here/preTreatment
-#make
-#cd $here/bin
+cd $here/preTreatment
+make
+cd $here/bin
 ./preTreatment R2019_05_02-11_48_23-000.root
 
 ###########
@@ -37,12 +37,12 @@ export here="/home/mlehuraux/GitLab/T2K/T2KNewElectronics/Analysis_Display_Soft"
 ###########
 #cd $here/display
 #make
-cd $here/bin
-for value in `seq 0 1 1357`
-do
-echo $value
-./display R2019_05_02-11_48_23-000.root $value
-done
+#cd $here/bin
+#for value in `seq 0 1 1357`
+#do
+#echo $value
+#./display R2019_05_02-11_48_23-000.root $value
+#done
 
 
 
