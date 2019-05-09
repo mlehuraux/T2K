241cd93db6c05f1d65757ffab2e68a5cc0949747
Marion LEHURAUX <marion.lehuraux@hotmail.fr>
2019-05-09 15:09:33 +0200

    Update

--- T2KNewElectronics/Analysis_Display_Soft/src/DAQ.cxx
+++ T2KNewElectronics/Analysis_Display_Soft/src/DAQ.cxx
@@ -11,28 +11,14 @@ void DAQ::loadDAQ()
 {
     int det, arc;
 
-    ifstream det2arc0((loc::daq + "detector2arc.txt").c_str());
-    while (!det2arc0.eof())
+    ifstream det2arc((loc::daq + "detector2arc.txt").c_str());
+    while (!det2arc.eof())
     {
-        det2arc0 >> det >> arc;
+        det2arc >> det >> arc;
         detector2arc[det]=arc;
         arc2detector[arc]=det;
-        //detector2arc[2][det]=arc;
-        //arc2detector[2][arc]=det;
     }
-    det2arc0.close();
-/*
-    ifstream det2arc1((loc::daq + "detector2arc1.txt").c_str());
-    while (!det2arc1.eof())
-    {
-        det2arc1 >> det >> arc;
-        detector2arc[1][det]=arc;
-        arc2detector[1][arc]=det;
-        detector2arc[3][det]=arc;
-        arc2detector[3][arc]=det;
-    }
-    det2arc1.close();
-*/
+    det2arc.close();
 
     int daq;
     ifstream farc2daq((loc::daq + "arc2daq.txt").c_str());
